4bfe5a315672240ca741318b3dc22e4e596e0abc
gn08 <gabbyfilipova@gmail.com>
2024-10-25 15:21:55 -0700

    commit 3:20

--- main.cpp
+++ main.cpp
@@ -13,6 +13,65 @@ void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
 
+int main() {
+    bool again;
+    int choice;
+    bool valid_choice;
+    srand(time(0));
+
+    do{
+        cout << "\n*** GOAT MANAGER 3001 ***" << endl;
+        cout << "[1] Add a goat" << endl;
+        cout << "[2] Delete a goat" << endl;
+        cout << "[3] List goats" << endl;
+        cout << "Choice -->";
+
+        cin >> choice;
+
+        if (cin.fail() || choice < 1 || choice > 4){
+            cin.clear();
+            cin.ignore(1000, '/n');
+            cout << "Invalid" << endl;
+        } else {
+            valid_choice = true;
+        }
+    }while (!valid_choice);
+    return choice;
+
+    // read & populate arrays for names and colors
+    ifstream fin("names.txt");
+    string names[SZ_NAMES];
+    int i = 0;
+    while (fin >> names[i++]);
+    fin.close();
+    ifstream fin1("colors.txt");
+    string colors[SZ_COLORS];
+    i = 0;
+    while (fin1 >> colors[i++]);
+    fin1.close();
+
+    list<Goat> trip;
+    bool quit = false;
+    while (!quit){
+        int choice = main_menu();
+        switch (choice){
+            case 1:
+                add_goat(trip, names, colors);
+                break;
+            case 2:
+                delete_goat(trip);
+                break;
+            case 3:
+                display_trip(trip);
+                break;
+            case 4:
+                quit = true;
+                break;
+        }
+    }
+    return 0;
+}
+
 void add_goat(list<Goat>trip, string names[], string colors[]){
     int name_index = rand() % SZ_NAMES;
     int color_index = rand() % SZ_COLORS;
@@ -70,68 +129,3 @@ int select_goat(list<Goat>trip){
 
     return selection 1;
 }
-
-int main() {
-    list<Goat> trip;
-    string names[SZ_NAMES];
-    string colors[SZ_COLORS];
-
-    bool quit = false;
-    while (!quit){
-        int choice = main_menu();
-        switch (choice){
-            case 1:
-                add_goat(trip, names, colors);
-                break;
-            case 2:
-                delete_goat(trip);
-                break;
-            case 3:
-                display_trip(trip);
-                break;
-            case 4:
-                quit = true;
-                break;
-        }
-        return 0;
-    }
-    srand(time(0));
-    bool again;
-
-    do{
-        cout << "\n*** GOAT MANAGER 3001 ***" << endl;
-        cout << "[1] Add a goat" << endl;
-        cout << "[2] Delete a goat" << endl;
-        cout << "[3] List goats" << endl;
-        cout << "Choice -->";
-
-        cin >> choice;
-
-        if (cin.fail() || choice < 1 || choice > 4){
-            cin.clear();
-            cin.ignore(1000, '/n');
-            cout << "Invalid" << endl;
-        } else {
-            valid_choice = true;
-        }
-    }while (!valid_choice);
-    return choice;
-
-    // read & populate arrays for names and colors
-    ifstream fin("names.txt");
-    string names[SZ_NAMES];
-    int i = 0;
-    while (fin >> names[i++]);
-    fin.close();
-    ifstream fin1("colors.txt");
-    string colors[SZ_COLORS];
-    i = 0;
-    while (fin1 >> colors[i++]);
-    fin1.close();
-
-
-
-
-    return 0;
-}
-
